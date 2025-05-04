b76ac4d6cc35251cd7d2745a4595d877d7af185d
marketeyeoptions <marketeyeoptions@gmail.com>
2025-05-05 00:45:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import requests
 from datetime import datetime
 
-# إعدادات تيليجرام
 TELEGRAM_BOT_TOKEN = "7613977084:AAF-65aYBx_YJcF_f8Xf9PaaqE7AZ1FUjI4"
 TELEGRAM_CHAT_ID = "@marketeyeoptions"
-
-# مفتاح API من Polygon
 POLYGON_API_KEY = "BwIqC9PU9vXhHDympuBEb3_JLE4_FWIf"
 
 def fetch_option_price():
     url = f"https://api.polygon.io/v3/reference/options/contracts/NVDA250509C00115000?apiKey={POLYGON_API_KEY}"
     response = requests.get(url)
+
     print(f"Status: {response.status_code}, Response: {response.text}")
+    
     if response.status_code == 200:
         data = response.json()
-        price = data.get("results", {}).get("price", {}).get("last", {}).get("price", "N/A")
+        price = data["results"]["price"]
         return price
     else:
         return None
@@ -33,7 +32,7 @@ def send_telegram_message(message):
 def main():
     price = fetch_option_price()
     if price is not None:
-        msg = f"[{datetime.now()}] NVDA 115C (2025-05-09) Last Price: {price}"
+        msg = f"[{datetime.now()}] NVDA Call 115 (2025-05-09) Bid: {price}"
     else:
         msg = f"[{datetime.now()}] Failed to fetch option price."
     print(msg)
